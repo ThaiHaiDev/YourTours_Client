c49622197ff5dde7e527c89a7340ea9f89fc2217
Nguyễn Thái Hải <85157423+ThaiHaiDev@users.noreply.github.com>
2022-10-26 11:46:59 +0700

    fix css barner advertisement, policy

--- src/components/Advertisement/Advertisement.tsx
+++ src/components/Advertisement/Advertisement.tsx
@@ -12,13 +12,41 @@ export default function SimpleSlider() {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         autoplay: true,
-        autoplaySpeed: 8000,
+        autoplaySpeed: 5000,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
     return (
         <div className="slider__adversiment">
+            <h1>Ưu đãi hấp dẫn</h1>
             <Slider {...settings}>
                 <div>
                     <img
---
+++ src/components/Policy/Policy.tsx
@@ -0,0 +1,29 @@
+import './Policy.scss';
+
+const Policy = () => {
+    return (
+        <div className="policy">
+            <h1>Vì sao bạn nên chọn Klook</h1>
+            <div className="row">
+                <div className="col l-3">
+                    <p>Nhiều lựa chọn</p>
+                    <span>Tìm kiếm niềm vui của bạn với gần nửa triệu điểm tham quan, khách sạn và hơn thế nữa.</span>
+                </div>
+                <div className="col l-3">
+                    <p>Ưu đãi giá tốt</p>
+                    <span>Dịch vụ hàng đầu. Giá tốt. Và nhận Klook credit để được giảm giá cho đơn hàng tiếp theo.</span>
+                </div>
+                <div className="col l-3">
+                    <p>Dễ dàng khám phá</p>
+                    <span>Đặt hàng phút chót, bỏ qua hàng chờ và được miễn phí hủy để tận hưởng trọn vẹn.</span>
+                </div>
+                <div className="col l-3">
+                    <p>Đáng tin cậy</p>
+                    <span>Đọc đánh giá chân thực và được hỗ trợ tận tình. Chúng tôi sẽ phục vụ bạn từng bước.</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Policy
