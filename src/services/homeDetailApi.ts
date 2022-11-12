c4abb9e3ff451f46540a6867ef84c586585d1455
Nguyễn Thái Hải <85157423+ThaiHaiDev@users.noreply.github.com>
2022-11-12 10:11:22 +0700

    update call api get list home of host

--- src/services/homeDetailApi.ts
+++ src/services/homeDetailApi.ts
@@ -1,17 +1,14 @@
-// import axios, { AxiosResponse } from "axios";
-import axiosClient from "../share/axios-client/axiosClient";
-import { CreateHomeDetailResquest } from "../share/models/homeDetail";
-
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+import axiosClient from '../share/axios-client/axiosClient';
+import { CreateHomeDetailResquest } from '../share/models/homeDetail';
 
 const homeDetailApi = {
-    // getRoomCategory(data: any): Promise<AxiosResponse> {
-    //     const url = `${API_BASE_URL}api/v1/auth/register`;
-    //     return axiosClient.post(url, data);
-    // },
-    createHomeDetailByHost(data : CreateHomeDetailResquest): Promise<any> {
-        const url = "api/v1/cms/homes/create";
-        return axiosClient.post(url,data);
+    createHomeDetailByHost(data: CreateHomeDetailResquest): Promise<any> {
+        const url = 'api/v1/cms/homes/create';
+        return axiosClient.post(url, data);
+    },
+    getListHomeOfHost(): Promise<any> {
+        const url = `api/v1/cms/homes/page`;
+        return axiosClient.get(url);
     },
 };
 
