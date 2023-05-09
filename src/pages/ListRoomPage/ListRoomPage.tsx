a05ee1a0a410c34d970f29c6516612727e475995
ThaiHaiDev <thaihai1004@gmail.com>
2023-05-09 23:22:14 +0700

    fix reload call api listRoomPage

--- src/pages/ListRoomPage/ListRoomPage.tsx
+++ src/pages/ListRoomPage/ListRoomPage.tsx
@@ -1,17 +1,17 @@
-import FilterBar from '../../components/FilterBar/FilterBar';
-import './ListRoomPage.scss';
-
-import InfiniteScroll from 'react-infinite-scroll-component';
 import { useEffect, useMemo, useState } from 'react';
-import NavbarFix from '../../components/NavbarFix/NavbarFix';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+import { useLocation } from 'react-router-dom';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 // Import css files
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import FilterBar from '../../components/FilterBar/FilterBar';
+import NavbarFix from '../../components/NavbarFix/NavbarFix';
 import RoomItem from '../../components/RoomItem/RoomItem';
 import SkeletonRoomItem from '../../components/Skeleton/SkeletonRoomItem';
 import filterApi from '../../services/filterApi';
-import { useLocation } from 'react-router-dom';
+import './ListRoomPage.scss';
 
 const ListRoomPage = () => {
     const [listDataRoom, setListDataRoom] = useState<any>([]);
@@ -29,40 +29,64 @@ const ListRoomPage = () => {
 
     const [state, setState] = useState<any>({
         items: Array.from({ length: 8 }),
-        hasMore: true
+        hasMore: true,
     });
 
     useEffect(() => {
         setLoading(true);
-        filterApi.getAllRoomsWithFilter({queryParams: queryParams !== '' ? queryParams : '', pagi : state.items.length}).then((dataResponse: any) => {
-            setListDataRoom(dataResponse.data.content);
-            setLoading(false);
-        });
+        filterApi
+            .getAllRoomsWithFilter({ queryParams: queryParams !== '' ? queryParams : '', pagi: state.items.length })
+            .then((dataResponse: any) => {
+                setListDataRoom(dataResponse.data.content);
+                setLoading(false);
+            });
     }, [queryParams, state.items.length]);
-    // console.log(state.items.length, listDataRoom.length)
+    console.log(state.items.length, listDataRoom.length);
 
     const fetchMoreData = () => {
         // a fake async api call like which sends
         // 20 more records in 1.5 secs
+        // setTimeout(() => {
+        //     if (state.items.length - listDataRoom.length <= 8) {
+        //         setState((prevState: any) => {
+        //             const newItems = Array.from({ length: 8 });
+        //             const items = prevState.items.concat(newItems);
+        //             return {
+        //                 items,
+        //                 hasMore: items.length > listDataRoom.length,
+        //             };
+        //         });
+        //     }
+        // }, 1500);
         setTimeout(() => {
-            if ((state.items.length - listDataRoom.length ) <= 8) {
-                setState({
-                    items: state.items.concat(Array.from({ length: 8 })),
-                });
+            if (listDataRoom.length < state.items.length) {
+                // Không cần lấy thêm dữ liệu
+                setState((prevState: any) => ({
+                    ...prevState,
+                    hasMore: false,
+                }));
+                return;
             }
+
+            // Lấy thêm 8 phần tử tiếp theo
+            const newItems = Array.from({ length: 8 });
+            setState((prevState: any) => ({
+                items: prevState.items.concat(newItems),
+                hasMore: true,
+            }));
         }, 1500);
     };
     return (
         <div className="list-room__page">
-            <div style={{ position: 'fixed', zIndex: '99'}}>
+            <div style={{ position: 'fixed', zIndex: '99' }}>
                 <NavbarFix />
-                <FilterBar filterData={filterData} queryParams={queryParams} pagi={state.items.length}/>
+                <FilterBar filterData={filterData} queryParams={queryParams} pagi={state.items.length} />
             </div>
             <div>
                 <InfiniteScroll
-                    dataLength={state.items.length}
+                    dataLength={listDataRoom.length}
                     next={fetchMoreData}
-                    hasMore={true}
+                    hasMore={state.hasMore}
                     loader={false}
                     scrollableTarget="scrollableDiv"
                     style={{ paddingTop: '160px', zIndex: '-1', margin: '0 100px' }}
