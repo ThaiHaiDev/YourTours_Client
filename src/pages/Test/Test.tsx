7c02b7f891d4bd5d1680acba40b40f61b165b01b
ThaiHaiDev <thaihai1004@gmail.com>
2023-06-03 17:14:55 +0700

    test bell mp3

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,6 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
+
+import Bell from '../../assets/img/bell.jpg';
+import audioFile from '../../assets/music/2.mp3';
+import './Test.scss';
+
+// const getAccessTokenFromLocalStorage = (): any => {
+//     return localStorage.getItem('access_token') || '{}';
+// };
 
 const Test = () => {
     const [connected, setConnected] = useState<any>(false);
@@ -8,6 +16,35 @@ const Test = () => {
     const [name, setName] = useState<any>('');
 
     var stompClient: any = null;
+
+    const audioRef = useRef<any>(null);
+
+    useEffect(() => {
+        // audioRef.current.play();
+        // audioRef.current.play().catch((error: any) => {
+        //     console.log(error);
+        //     // Autoplay was prevented, handle the error or show a user interaction prompt.
+        // });
+        const playAudio = () => {
+            audioRef.current.play().catch((error: any) => {
+                // Autoplay was prevented, handle the error or show a user interaction prompt.
+            });
+        };
+
+        // Trigger playAudio() after a user interaction event, such as a button click.
+        document.addEventListener('click', playAudio);
+
+        return () => {
+            // Clean up the event listener when the component unmounts.
+            document.removeEventListener('click', playAudio);
+        };
+    }, []);
+
+    const handlePlay = () => {
+        audioRef.current.play().catch((error: any) => {
+            // Autoplay was prevented, handle the error or show a user interaction prompt.
+        });
+    };
 
     const setConnectedStatus = (connected: any) => {
         setConnected(connected);
@@ -37,10 +74,14 @@ const Test = () => {
         stompClient.connect({}, function (frame: any) {
             setConnectedStatus(true);
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/greetings', function (greeting: any) {
+            // stompClient.subscribe('/topic/greetings', function (greeting: any) {
+            //     showGreeting(JSON.parse(greeting.body));
+            // });
+            // stompClient.send('/app/hello', {}, JSON.stringify({ name: name }));
+            stompClient.subscribe('/topic/notification/6f7098fe-ad04-47a4-8e3c-ff4722328d0b', function (greeting: any) {
                 showGreeting(JSON.parse(greeting.body));
+                console.log('check', greeting.body);
             });
-            stompClient.send('/app/hello', {}, JSON.stringify({ name: name }));
         });
     };
 
@@ -94,6 +135,12 @@ const Test = () => {
                     ))}
                 </tbody>
             </table>
+
+            <h1 className="ring">Tesssstttt</h1>
+            <img src={Bell} alt="" className="bell" onClick={handlePlay} />
+            <audio src={audioFile} controls ref={audioRef} style={{ display: 'none' }}>
+                test
+            </audio>
         </div>
     );
 };
