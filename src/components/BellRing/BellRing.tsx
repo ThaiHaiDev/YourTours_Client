8f98eb40ac3f14dced631f19508feb8090929f82
ThaiHaiDev <thaihai1004@gmail.com>
2023-06-06 22:32:23 +0700

    improve ui nav with bell noti

---
+++ src/components/BellRing/BellRing.tsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react';
+
+import './BellRing.scss';
+
+const clickOutsideRef = (content_ref: any, toggle_ref: any) => {
+    document.addEventListener('mousedown', (e) => {
+        // user click toggle
+        if (toggle_ref.current && toggle_ref.current.contains(e.target)) {
+            content_ref.current.classList.toggle('active');
+        } else {
+            // user click outside toggle and content
+            if (content_ref.current && !content_ref.current.contains(e.target)) {
+                content_ref.current.classList.remove('active');
+            }
+        }
+    });
+};
+
+const BellRing = (props: any) => {
+    const dropdown_toggle_el = useRef(null);
+    const dropdown_content_el = useRef(null);
+
+    clickOutsideRef(dropdown_content_el, dropdown_toggle_el);
+
+    return (
+        <div className="bell-ring">
+            <button ref={dropdown_toggle_el} className="dropdown__toggle">
+                {props.icon ? <i className={props.icon}></i> : ''}
+                {props.badge ? <span className="dropdown__toggle-badge">{props.badge}</span> : ''}
+                {props.customToggle ? props.customToggle() : ''}
+            </button>
+            <div ref={dropdown_content_el} className="dropdown__content">
+                {props.contentData && props.renderItems
+                    ? props.contentData.map((item: any, index: any) => props.renderItems(item, index))
+                    : ''}
+                {props.renderFooter ? <div className="dropdown__footer">{props.renderFooter()}</div> : ''}
+            </div>
+        </div>
+    );
+};
+
+export default BellRing;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,25 @@
 import { useState, useRef, useEffect } from 'react';
+import { t } from 'i18next';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+
+import Logo from '../../assets/imgMaster/logo.svg';
+import notifications from '../../mockdata/notification.json';
+
+import { RootState } from '../../redux/store';
+import BellRing from '../BellRing/BellRing';
 import Book from '../Book/Book';
 import DropdownUser from '../DropdownUser/DropdownUser';
-import Logo from '../../assets/imgMaster/logo.svg';
+import LanguageSelected from '../LanguageSelected/LanguageSelected';
+
 import './Navbar.scss';
 
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
-import { t } from 'i18next';
-import LanguageSelected from '../LanguageSelected/LanguageSelected';
+const renderNotificationItem = (item: any, index: any) => (
+    <div className="notification-item" key={index}>
+        <i className={`${item.icon} notification-icon`}></i>
+        <span className="notification-content">{item.content}</span>
+    </div>
+);
 
 const Navbar = () => {
     const [isActive, setIsActive] = useState<boolean>(false);
@@ -35,7 +46,8 @@ const Navbar = () => {
                         <img src={Logo} alt="company logo" className="logo-bg" />
                     </div>
                 </NavLink>
-                <div className="navbar-right menu">
+                <div className="navbar-right menu" style={{ display: 'flex' }}>
+                    <LanguageSelected />
                     <NavLink to="/" end={true}>
                         {t('navbar.home')}
                     </NavLink>
@@ -49,7 +61,7 @@ const Navbar = () => {
                     )}
                 </div>
 
-                <div className="navbar-right">
+                <div className="navbar-right" style={{ display: 'flex' }}>
                     {user.current?.id !== undefined ? (
                         <DropdownUser />
                     ) : (
@@ -57,7 +69,13 @@ const Navbar = () => {
                             <NavLink to="/signin">{t('navbar.signin')}</NavLink>
                         </>
                     )}
-                    <LanguageSelected />
+                    <BellRing
+                        icon="bx bx-bell"
+                        badge="12"
+                        contentData={notifications}
+                        renderItems={(item: any, index: any) => renderNotificationItem(item, index)}
+                        renderFooter={() => <p>View All</p>}
+                    />
                 </div>
             </div>
             {isActive && <Book refOne={refOne} />}
